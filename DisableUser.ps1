bacf91c80088e381abd6bef8e01cd4342ff3fcec
Nodocchi <zuljinraynor@gmail.com>
2018-08-17 16:16:48 -0400

    DisableUser Script Added a script that disables an AD user, prints all the groups the User was in into a CSV file and then removes the user from those groups.

---
+++ DisableUser.ps1
@@ -0,0 +1,27 @@
+﻿param (
+    [Alias("User","U")][string]$UserName,
+    [switch]$Verbose
+
+)
+
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+. $ScriptDir\EnvConfig.ps1
+
+Write-Host "Disable a User Account - on prem"
+
+If (!$UserName) {$UserName = Read-Host "Enter username"}
+
+Disable-ADAccount -Identity $UserName
+
+$Groups = Get-ADPrincipalGroupMembership $UserName
+$Groups | Select NAME |  Export-Csv $UserGroupCSVLocation\$UserName.csv -Force
+
+foreach ($Group in $Groups) {
+    if ($Group.name -ne "Domain Users") {
+        Remove-ADGroupMember -Identity $Group -Members $UserName -Confirm:$false;
+        if($Verbose) { write-host "Removed" $username "from Group:" $group.name; }
+    }
+}
+
+
+
