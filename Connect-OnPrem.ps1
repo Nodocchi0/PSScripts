bad93030b796e32570513952e7e05d4c69582daa
Nodocchi <zuljinraynor@gmail.com>
2018-08-23 15:25:36 -0400

    Added variable loading to Connect-OnPrem Forgot to have the Connect-OnPrem script load the variable file. Have corrected this mistake.

--- Connect-OnPrem.ps1
+++ Connect-OnPrem.ps1
@@ -1,3 +1,5 @@
-﻿$UserCredential = Get-Credential
+﻿$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+. $ScriptDir\EnvConfig.ps1
+$UserCredential = Get-Credential
 $Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $onPremURI -Authentication Kerberos -Credential $UserCredential
 Import-PSSession $Session
