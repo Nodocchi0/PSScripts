dcb34525058c5be92872d5101ece8d36285db303
Nodocchi <zuljinraynor@gmail.com>
2018-08-28 14:11:21 -0400

    Created RemoveCalendarAccess script Simple because it simply calls Remove-MailboxFolderPermission, which already does what this script is trying to do, but makes it a bit easier to work with if you are already in PowerShell and messing around with mailboxes.

---
+++ RemoveCalendarAccess.ps1
@@ -0,0 +1,11 @@
+﻿param (
+    [Alias("Username","U")][string]$User,
+    [Alias("Cal","C")][string]$Calendar
+)
+
+Write-Host "Remove permission for user to use a calendar. - Office 365"
+
+If (!$User) {$User = Read-Host "Enter username of user you want to remove access to"}
+If (!$Calendar) {$Calendar = Read-Host "Enter user who's calendar you want to unshare"}
+
+Remove-MailboxFolderPermission -Identity $Calendar`:\Calendar -User $User -Confirm:$false
