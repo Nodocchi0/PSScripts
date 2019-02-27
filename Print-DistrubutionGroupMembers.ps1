ceab1cfd2afd19820fafcb896eb87338e24dec3c
Nodocchi <zuljinraynor@gmail.com>
2019-02-27 09:33:52 -0500

    Print all members in an AD group Exports to CSV, and has toggle for recursive or not.

---
+++ Print-DistrubutionGroupMembers.ps1
@@ -0,0 +1,14 @@
+﻿param (
+    [Alias("G")][string]$Group,
+    [switch]$Recursive
+)
+
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+. $ScriptDir\EnvConfig.ps1
+
+Write-Host "Print all Users in a distribution list"
+
+If (!$Group) {$Group = Read-Host "Enter group name: "}
+
+if ($Recursive) { get-adgroupmember -identity $group -Recursive | select name | export-csv ($LocalExportCSVLocation + $Group + ".csv") -NoTypeInformation }
+else { get-adgroupmember -identity $group | select name | export-csv ($LocalExportCSVLocation + $Group + ".csv") -NoTypeInformation }
