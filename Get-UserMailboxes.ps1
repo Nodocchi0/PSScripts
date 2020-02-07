4b627b6ea143dea6dd9d75b1b24aa1b0ccc45f1a
Nodocchi <zuljinraynor@gmail.com>
2020-02-07 09:50:20 -0500

    Get-UserMailboxes Added a simple script to get all the mailboxes a user has access to. The command is simple but sometimes I could forget where I have to pipe what so I just made this simple one.

---
+++ Get-UserMailboxes.ps1
@@ -0,0 +1,10 @@
+﻿ param (
+    [Alias("User","U")][string]$UserName
+)
+
+$ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
+. $ScriptDir\EnvConfig.ps1
+
+Write-Host "All Shared Mailboxes for User. (This may take a while)."
+
+Get-Mailbox | Get-MailboxPermission -User $UserName
